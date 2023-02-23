86823d171e36a58ece52441fbf9ae556ff23a6d9
karina <karyna.rein@gmail.com>
2023-02-23 11:19:28 +0300

    50

---
+++ Talk 50/Program.cs
@@ -0,0 +1,56 @@
+﻿int GetNumber(string message)
+{
+    int number = 0;
+    while (true)
+    {   Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out number)&& number>=0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некорректное число. Повторите ввод.");
+        }
+    }
+    return number;
+} 
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,]matrix=new int[rows,columns];
+    Random rnd=new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j]=rnd.Next(1,10);
+        }
+    }
+    return matrix;
+}
+
+void CheckMatrix(int[,]matri, int a, int b)
+{    
+    if(a>=matri.GetLength(0) && b>=matri.GetLength(1))
+    Console.WriteLine("Такого элемента нет");
+    else
+    Console.WriteLine($"Элемент массива равен {matri[a,b]}");
+        
+}
+void PrintMatrix(int[,]matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m=GetNumber("Введите число");
+int n=GetNumber("Введите число");
+int[,]matrix=InitMatrix(5,5);
+PrintMatrix(matrix);
+CheckMatrix(matrix,m,n);
